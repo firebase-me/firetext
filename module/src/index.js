82582124cf3957031b3621c3bff92020317b27ed
digimbyte <digimbyte@gmail.com>
2021-05-22 15:05:00 +1000

    reformatted

---
+++ module/src/index.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const IndexTable = {};
+
+const initiate = ()=>{
+
+}
+
+
+const find = (collection, input) =>{
+    IndexTable[collection]
+};
+
+export default {find, initiate}
